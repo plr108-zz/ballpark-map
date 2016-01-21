fc8f4714c3b573b2913e027258ec2212ec0bb696
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-21 17:51:24 -0500

    Add PNC Park marker

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-var map;
+var initialBallparks = [{
+    name: "PNC Park",
+    lat: 40.4470471765,
+    lng: -80.0061745423
+}];
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    // coordinates to show entire US on a 1600x900px display
-    center: {lat: 38, lng: -96},
-    zoom: 5
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        // coordinates to show entire US on a 1600x900px display
+        center: {
+            lat: 40.4470471765,
+            lng: -80.0061745423
+        },
+        zoom: 17
+    });
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {
+            lat: 40.4470471765,
+            lng: -80.0061745423
+        },
+        title: 'PNC Park'
+    });
 }
