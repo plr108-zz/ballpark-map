2f274787274137f6ec33ff5965aad67406ca1674
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-22 10:31:27 -0500

    Add knockout bindings, ViewModel and list of ballpark names

--- js/app.js
+++ js/app.js
@@ -1,9 +1,40 @@
-var initialBallparks = [{
-    name: "PNC Park",
-    lat: 40.4470471765,
-    lng: -80.0061745423
-}];
+var initialBallparks = [
+	{
+		title: 'PNC Park',
+        lat: 40.4470471765,
+        lng: -80.0061745423
+	},
+	{
+		title: 'Fakie Field',
+        lat: 33,
+        lng: -75
+	}
+];
 
+var Ballpark = function(data) {
+	this.title = ko.observable(data.title);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+};
+
+var ViewModel = function() {
+	var self = this;
+
+ 	this.ballparkList = ko.observableArray([]);
+
+ 	initialBallparks.forEach(function(ballparkItem) {
+ 		self.ballparkList.push(new Ballpark(ballparkItem));
+
+ 	});
+
+ 	this.getMapMarker = function() {
+ 		return {}
+ 	};
+};
+
+ko.applyBindings(new ViewModel());
+
+// Google Maps callback function
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         // coordinates to show entire US on a 1600x900px display
@@ -11,8 +42,10 @@ function initMap() {
             lat: 40.4470471765,
             lng: -80.0061745423
         },
-        zoom: 17
+        zoom: 4
     });
+
+    console.log(ViewModel.ballparkList);
 
     var marker = new google.maps.Marker({
         map: map,
@@ -22,4 +55,4 @@ function initMap() {
         },
         title: 'PNC Park'
     });
-}
+};
