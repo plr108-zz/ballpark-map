5de0b33cf35293a00c1aca0505a89d15738b29c0
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-23 14:45:08 -0500

    Refactor to use window.initMap = function(){}

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+// Google Maps callback function
+window.initMap = function() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        // coordinates to show entire US on a 1600x900px display
+        center: {
+            lat: 41.5,
+            lng: -80.5
+        },
+        zoom: 6
+    });
+
+    // create map markers for all ballparks
+    for (i = 0; i < ballparks.length; i++) {
+        var marker = new google.maps.Marker({
+            map: map,
+            title: ballparks[i].title,
+            position: new google.maps.LatLng(ballparks[i].lat, ballparks[i].lng)
+        });
+    }
+
+};
+
 var ballparks = [{
     title: 'PNC Park',
     lat: 40.4470471765,
@@ -53,25 +75,3 @@ var ViewModel = function() {
 };
 
 ko.applyBindings(new ViewModel());
-
-// Google Maps callback function
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        // coordinates to show entire US on a 1600x900px display
-        center: {
-            lat: 41.5,
-            lng: -80.5
-        },
-        zoom: 6
-    });
-
-    // create map markers for all ballparks
-    for (i = 0; i < ballparks.length; i++) {
-        var marker = new google.maps.Marker({
-            map: map,
-            title: ballparks[i].title,
-            position: new google.maps.LatLng(ballparks[i].lat, ballparks[i].lng)
-        });
-    }
-
-};
