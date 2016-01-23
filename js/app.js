be7385413b4d7699f62876fbca6e380db5d7f1f8
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-22 19:17:38 -0500

    Create map markers from ballparks model via for loop

--- js/app.js
+++ js/app.js
@@ -1,35 +1,40 @@
-var initialBallparks = [
-	{
-		title: 'PNC Park',
-        lat: 40.4470471765,
-        lng: -80.0061745423
-	},
-	{
-		title: 'Fakie Field',
-        lat: 33,
-        lng: -75
-	}
-];
+var ballparks = [{
+    title: 'PNC Park',
+    lat: 40.4470471765,
+    lng: -80.0061745423
+}, {
+    title: 'Fakie Field',
+    lat: 33,
+    lng: -75
+}];
 
 var Ballpark = function(data) {
-	this.title = ko.observable(data.title);
-	this.lat = ko.observable(data.lat);
-	this.lng = ko.observable(data.lng);
+    this.title = ko.observable(data.title);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
 };
 
 var ViewModel = function() {
-	var self = this;
+    var self = this;
 
- 	this.ballparkList = ko.observableArray([]);
+    this.ballparkList = ko.observableArray([]);
 
- 	initialBallparks.forEach(function(ballparkItem) {
- 		self.ballparkList.push(new Ballpark(ballparkItem));
+    ballparks.forEach(function(ballparkItem) {
+        self.ballparkList.push(new Ballpark(ballparkItem));
 
- 	});
+    });
 
- 	this.getMapMarker = function() {
- 		return {}
- 	};
+    this.getMarker = function() {
+        var markerJSON = {
+            map: map,
+            position: {
+                lat: 33,
+                lng: -75
+            },
+            title: 'Fakie Field'
+        };
+        return markerJSON;
+    };
 };
 
 ko.applyBindings(new ViewModel());
@@ -45,14 +50,13 @@ function initMap() {
         zoom: 4
     });
 
-    console.log(ViewModel.ballparkList);
+    // create map markers for all ballparks
+    for (i = 0; i < ballparks.length; i++) {
+        var marker = new google.maps.Marker({
+            map: map,
+            title: ballparks[i].title,
+            position: new google.maps.LatLng(ballparks[i].lat, ballparks[i].lng)
+        });
+    }
 
-    var marker = new google.maps.Marker({
-        map: map,
-        position: {
-            lat: 40.4470471765,
-            lng: -80.0061745423
-        },
-        title: 'PNC Park'
-    });
 };
