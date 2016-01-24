1fccce74ccd04cf8a31d452163e10b24e616defb
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-24 15:32:57 -0500

    Add activeBallpark functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+// Model //////////////////////////////////////////////////////////////////////
+var ballparks = [{
+    title: 'PNC Park',
+    lat: 40.4470471765,
+    lng: -80.0061745423
+}, {
+    title: 'Citizens Bank Park',
+    lat: 39.905569,
+    lng: -75.166591
+}, {
+    title: 'Oriole Park at Camden Yards',
+    lat: 39.283505,
+    lng: -76.621911
+}, {
+    title: 'Progressive Field',
+    lat: 41.495537,
+    lng: -81.685278
+}, {
+    title: 'U.S. Cellular Field',
+    lat: 41.830176,
+    lng: -87.634225
+}, {
+    title: 'Great American Ball Park',
+    lat: 39.097466,
+    lng: -84.507029
+}, {
+    title: 'Wrigley Field',
+    lat: 41.947902,
+    lng: -87.655823
+}];
+
+var Ballpark = function(data) {
+    this.title = ko.observable(data.title);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+};
+// End Model //////////////////////////////////////////////////////////////////
+
+// View ///////////////////////////////////////////////////////////////////////
 // Google Maps callback function
 window.initMap = function() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -20,58 +59,28 @@ window.initMap = function() {
 
 };
 
-var ballparks = [{
-    title: 'PNC Park',
-    lat: 40.4470471765,
-    lng: -80.0061745423
-},
-{
-    title: 'Citizens Bank Park',
-    lat: 39.905569,
-    lng: -75.166591
-},
-{
-    title: 'Oriole Park at Camden Yards',
-    lat: 39.283505,
-    lng: -76.621911
-},
-{
-    title: 'Progressive Field',
-    lat: 41.495537,
-    lng: -81.685278
-},
-{
-    title: 'U.S. Cellular Field',
-    lat: 41.830176,
-    lng: -87.634225
-},
-{
-    title: 'Great American Ball Park',
-    lat: 39.097466,
-    lng: -84.507029
-},
-{
-    title: 'Wrigley Field',
-    lat: 41.947902,
-    lng: -87.655823
-}
-];
+// End View ///////////////////////////////////////////////////////////////////
 
-var Ballpark = function(data) {
-    this.title = ko.observable(data.title);
-    this.lat = ko.observable(data.lat);
-    this.lng = ko.observable(data.lng);
-};
-
+// View Model /////////////////////////////////////////////////////////////////
 var ViewModel = function() {
+    // self is the ViewModel binding
     var self = this;
 
+    // self.ballparkList
     this.ballparkList = ko.observableArray([]);
+    this.activeBallpark = null;
 
     ballparks.forEach(function(ballparkItem) {
         self.ballparkList.push(new Ballpark(ballparkItem));
 
     });
+
+    this.activeBallpark = ko.observable(this.ballparkList()[0]);
+
+    this.setActiveBallpark = function(clickedBallpark) {
+        self.activeBallpark(clickedBallpark);
+    }
 };
 
 ko.applyBindings(new ViewModel());
+// End View Model /////////////////////////////////////////////////////////////
