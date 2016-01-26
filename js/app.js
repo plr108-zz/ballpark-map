c73f821a98a6f490d5aff050bdef8c7fc79f8520
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-26 14:44:06 -0500

    Add ability to show info on the map when a ballpark list item is clicked.

--- js/app.js
+++ js/app.js
@@ -1,57 +1,62 @@
 // Model //////////////////////////////////////////////////////////////////////
 var model = {
-    ballparks : [{
+    ballparks: [{
         title: 'PNC Park',
         lat: 40.4470471765,
-        lng: -80.0061745423
+        lng: -80.0061745423,
+        markerID: 0
     }, {
         title: 'Citizens Bank Park',
         lat: 39.905569,
-        lng: -75.166591
+        lng: -75.166591,
+        markerID: 1
     }, {
         title: 'Oriole Park at Camden Yards',
         lat: 39.283505,
-        lng: -76.621911
+        lng: -76.621911,
+        markerID: 2
     }, {
         title: 'Progressive Field',
         lat: 41.495537,
-        lng: -81.685278
+        lng: -81.685278,
+        markerID: 3
     }, {
         title: 'U.S. Cellular Field',
         lat: 41.830176,
-        lng: -87.634225
+        lng: -87.634225,
+        markerID: 4
     }, {
         title: 'Great American Ball Park',
         lat: 39.097466,
-        lng: -84.507029
+        lng: -84.507029,
+        markerID: 5
     }, {
         title: 'Wrigley Field',
         lat: 41.947902,
-        lng: -87.655823
+        lng: -87.655823,
+        markerID: 6
     }],
 };
+
+// Ballpark object used by KO
 var Ballpark = function(data) {
     this.title = ko.observable(data.title);
     this.lat = ko.observable(data.lat);
     this.lng = ko.observable(data.lng);
+    this.markerID = ko.observable(data.markerID);
 };
 
 // End Model //////////////////////////////////////////////////////////////////
 
 // View ///////////////////////////////////////////////////////////////////////
-//
-var mapView = {
 
-    // markers will be used to track the map markers
-    markers : []
-
-    // TODO: populate markers[] via ViewModel
-};
+// markers[] will be used to track the map markers
+var markers = [];
 
 // Google Maps callback function
 window.initMap = function() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        // coordinates to show entire US on a 1600x900px display
+        // coordinates are in the center of current ballparks
         center: {
             lat: 41.5,
             lng: -80.5
@@ -64,15 +69,18 @@ window.initMap = function() {
 
     // create map markers for all ballparks
     for (i = 0; i < model.ballparks.length; i++) {
-        var marker = new google.maps.Marker({
+        markers[i] = new google.maps.Marker({
             map: map,
             title: model.ballparks[i].title,
             position: new google.maps.LatLng(model.ballparks[i].lat, model.ballparks[i].lng),
             animation: google.maps.Animation.DROP
         });
 
+        // set markerID
+        model.ballparks[i].markerID = i;
+
         // create event listener for clicking the marker
-        google.maps.event.addListener(marker, 'click', (function(marker) {
+        google.maps.event.addListener(markers[i], 'click', (function(marker) {
             return function() {
                 // make the marker bounce for 750ms
                 setBounce(marker);
@@ -89,7 +97,7 @@ window.initMap = function() {
                 // open the infoWindow
                 infoWindow.open(map, marker);
             };
-        })(marker));
+        })(markers[i]));
 
         var initializeInfoWindow = function(marker, contentHTML, infoWindow) {
 
@@ -128,8 +136,14 @@ var ViewModel = function() {
 
     this.activeBallpark = ko.observable(this.ballparkList()[0]);
 
-    this.setActiveBallpark = function(clickedBallpark) {
-        self.activeBallpark(clickedBallpark);
+    this.setActiveBallpark = function(activeBallpark) {
+        // set KO binding
+        self.activeBallpark(activeBallpark);
+
+        // simulate marker click to show infoWindow containing activeBallpark info
+        google.maps.event.trigger(markers[activeBallpark.markerID()], 'click', {
+            latLng: new google.maps.LatLng(0, 0)
+        });
     }
 };
 
